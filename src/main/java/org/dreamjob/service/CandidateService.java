38b7bb18acbe59720e718eeeba3883cfc867e40d
Mistors1 <Kolov-dm@bk.ru>
2023-07-27 18:32:39 +0400

    DreamJob from repositories to services

---
+++ src/main/java/org/dreamjob/service/CandidateService.java
@@ -0,0 +1,20 @@
+package org.dreamjob.service;
+
+import org.dreamjob.model.Candidate;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface CandidateService {
+
+    Candidate save(Candidate candidate);
+
+    boolean deleteById(int id);
+
+    boolean update(Candidate candidate);
+
+    Optional<Candidate> findById(int id);
+
+    Collection<Candidate> findAll();
+
+}
---
+++ src/main/java/org/dreamjob/service/SimpleVacancyService.java
@@ -0,0 +1,47 @@
+package org.dreamjob.service;
+
+import org.dreamjob.model.Vacancy;
+import org.dreamjob.repository.MemoryVacancyRepository;
+import org.dreamjob.repository.VacancyRepository;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class SimpleVacancyService implements VacancyService {
+
+    private static final SimpleVacancyService INSTANCE = new SimpleVacancyService();
+
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+
+    private SimpleVacancyService() { }
+
+    public static SimpleVacancyService getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        return vacancyRepository.save(vacancy);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return vacancyRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancyRepository.update(vacancy);
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return vacancyRepository.findById(id);
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancyRepository.findAll();
+    }
+
+}
